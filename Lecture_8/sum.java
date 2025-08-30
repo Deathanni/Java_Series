3862fa42193a645ec60bc779449825aea20fdcbd
Deathanni <anni3therockstar@gmail.com>
2025-08-30 22:54:38 +0530

    Day 4 funtion more

--- Lecture_8/sum.java
+++ Lecture_8/sum.java
@@ -1,5 +1,7 @@
 public class sum {
     public static void main(String[] args) {
-        System.out.println("this is sum");
+        int a=10;
+        int b=20;
+        System.out.println(a+b);
     }
 }
