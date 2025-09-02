e436ab0805ba60a01095804be380544d34941a3c
Deathanni <anni3therockstar@gmail.com>
2025-09-02 21:30:05 +0530

    Day 8 Lecture 13 string Builder

--- Lecture_12/email_username.java
+++ Lecture_12/email_username.java
@@ -2,7 +2,7 @@ package Lecture_12;
 
 import java.util.Scanner;
 
-public class emai_username {
+public class email_username {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter you email");
---
+++ Lecture_13/StringBuilder_demo.java
@@ -0,0 +1,8 @@
+package Lecture_13;
+
+public class StringBuilder_demo {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("Hello");
+        System.out.println(sb);
+    }
+}
