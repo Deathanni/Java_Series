5e95e80fb986a98f5f23fe06030bf0853834f21a
Deathanni <anni3therockstar@gmail.com>
2025-09-02 17:40:11 +0530

    Day 8 Lecture 12

---
+++ Lecture_12/emai_username.java
@@ -0,0 +1,23 @@
+package Lecture_12;
+
+import java.util.Scanner;
+
+public class emai_username {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter you email");
+        String email=sc.nextLine();
+        String username="";
+
+        for (int i = 0; i < email.length(); i++) {
+            if(email.charAt(i)!='@'){
+                username=username+email.charAt(i);
+            }
+            else{
+                break;
+            }
+        }
+        System.out.println("The username is : "+ username);
+        sc.close();
+    }
+}
