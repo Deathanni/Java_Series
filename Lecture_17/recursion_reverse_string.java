6c98f79c87031171c01945adee227604f48e56f0
Deathanni <anni3therockstar@gmail.com>
2025-09-05 14:44:58 +0530

    Day 11 recursion ques

---
+++ Lecture_17/recursion_reverse_string.java
@@ -0,0 +1,21 @@
+package Lecture_17;
+
+import java.util.Scanner;
+
+public class recursion_reverse_string {
+    public static void reverseString(String str, int index) {
+        if(index==0){
+            System.out.print(str.charAt(index));
+            return;
+        }
+        System.out.print(str.charAt(index));
+        reverseString(str, index-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a string");
+        String str=sc.nextLine();
+        reverseString(str, str.length()-1);
+        sc.close();
+    }
+}
