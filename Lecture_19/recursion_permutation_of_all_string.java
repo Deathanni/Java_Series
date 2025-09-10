bd5f2fbbdf313725c8e6d956f603625be474dd3a
Deathanni <anni3therockstar@gmail.com>
2025-09-10 23:29:10 +0530

    Day 13 lecture 19

---
+++ Lecture_19/recursion_permutation_of_all_string.java
@@ -0,0 +1,25 @@
+package Lecture_19;
+
+public class recursion_permutation_of_all_string {
+    public static void printPerm(String str, String permutation) {
+        if (str.length() == 0) {
+            System.out.println(permutation);
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            char currChar = str.charAt(i);
+
+            // remove currChar from str
+            String newStr = str.substring(0, i) + str.substring(i + 1);
+
+            // call recursion with reduced string
+            printPerm(newStr, permutation + currChar);
+        }
+    }
+
+    public static void main(String args[]) {
+        String str = "abc";
+        printPerm(str, "");
+    }
+}
